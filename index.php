a044aba4da069f08ab5754403247c9f91d412991
Grégoire Daussin <gregoire.daussin@gmail.com>
2013-10-17 12:38:21 +0200

    Deisgnn

--- index.php
+++ index.php
@@ -4,20 +4,16 @@ require_once 'config.php';
 require_once 'utils.php';
 global $servers;
 ?><!doctype html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
 		<link rel="stylesheet" href="css/styles.css">
-		<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/app.js"></script>
 		<title>Git Control Center</title>
 	</head>
-	<body data-spy="scroll">
-
+	<body data-spy="scroll" data-target=".nav-list">
 		<div class="container">
 			<div class="row">
 				<div class="span3">
@@ -35,7 +31,8 @@ global $servers;
 						<h1><?php echo $pays; ?></h1>
 						<?php foreach ($ssh as $id => $param): ?>
 							<section id="<?php echo $id; ?>">
-								<h3><?php echo $param['title']; ?> <span class="label"></span></h3>
+								<h4><?php echo $param['title']; ?></h4>
+                                <div class="pull-right"><span class="label"></span></div>
 								<div class="btn-group">
 									<button class="btn" onclick="callRequest('status', '<?php echo $id; ?>');">
 										<i class="icon-refresh"></i> Status
@@ -43,20 +40,27 @@ global $servers;
 									<button class="btn" onclick="callRequest('pull', '<?php echo $id; ?>');">
 										<i class="icon-arrow-down"></i> Pull
 									</button>
-									<div class="input-prepend">
-										<button class="btn" type="button" onclick="callRequest('push', '<?php echo $id; ?>', document.querySelector('#<?php echo $id; ?> input[name=message]').value);">
-											<i class="icon-arrow-up"></i> Push
-										</button>
-										<input type="text" name="message" placeholder="Commit message">
-									</div>
 								</div>
 								<pre></pre>
+                                <div class="control-group">
+                                    <div class="controls">
+                                        <div class="input-append">
+                                            <input class="input-block-level" type="text" name="message" placeholder="Commit message">
+                                            <button class="btn" type="button" onclick="callRequest('push', '<?php echo $id; ?>', document.querySelector('#<?php echo $id; ?> input[name=message]').value);">
+                                                <i class="icon-arrow-up"></i> Push
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
 							</section>
 						<?php endforeach; ?>
+                        <hr />
 					<?php endforeach; ?>
 				</div>
 			</div>
 		</div>
-
+        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/app.js"></script>
 	</body>
 </html>
